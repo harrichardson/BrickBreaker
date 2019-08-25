a91f59c7ec949a49cc2861cfd03fce11c78dbeb0
Harry Richardson <harthoric@gmail.com>
2019-08-25 23:38:25 +0100

    Update KeyHandler.java

--- util/KeyHandler.java
+++ util/KeyHandler.java
@@ -1,17 +1,19 @@
 package com.harthoric.brickbreaker.util;
 
+import com.harthoric.brickbreaker.board.Board;
+
+import javafx.scene.input.KeyCode;
+
 public class KeyHandler {
 
-  public void checkKeys() {
+	public void checkKeys() {
 		if (isPressed(KeyCode.A) && Board.getPlayer().getX() < Board.WIDTH / 2 - 40)
 			Board.getPlayer().moveLeft(5);
 		else if (isPressed(KeyCode.D) && Board.getPlayer().getX() > -Board.WIDTH / 2 + 40)
 			Board.getPlayer().moveRight(5);
 	}
 
-
 	private boolean isPressed(KeyCode key) {
 		return Board.keys.getOrDefault(key, false);
 	}
-
 }
